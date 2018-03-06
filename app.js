9241de5fbe7314f9b075d92db24938fd05d11ee7
David <damccoy1@crimson.ua.edu>
2018-03-05 22:53:31 -0600

    problems

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.post("/checkout", function (req, res) {
 			var sa = require('superagent');
 			sa.post("https://ua-acm-web-util.herokuapp.com/member/payforsemester").send({purchaseID: result.transaction.id, size: shirtSize, email: userEmail, date: result.transaction.date}).end(function(err, res) {
 				console.log("bad thing happended");
-				res.send("something bad happened");
 			});
 			res.send("ok");
 		}
@@ -60,8 +59,6 @@ app.post("/checkout", function (req, res) {
 			res.send("bad");
 		}
 	});
-	console.log("what");
-	res.send("ok");
 });
 
 app.post("/validate", function(req, res) {
